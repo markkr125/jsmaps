57f443c7351b53ffcf2ecbd57d27a829c7d37c1b
markkr125 <mark125@gmail.com>
2016-01-12 22:03:57 +0200

    add width to map examples

--- jsfiles/exampleLoader.js
+++ jsfiles/exampleLoader.js
@@ -101,7 +101,7 @@ var ExampleLoader = {
 
         var html = '<?xml version="1.0" encoding="UTF-8"?>'+
             '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'+
-            '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head><title>Example</title><style>html, body, #map {height: 100%; margin: 0; padding: 0;}</style>';
+            '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head><title>Example</title><style>html, body, #map {width:100%;height: 100%; margin: 0; padding: 0;}</style>';
 
         for (var i in staticFiles) {
             if (staticFiles.hasOwnProperty(i) == false) continue;
