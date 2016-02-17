cb46480e2349794b37495999b3aadadfe35777fe
markkr125 <mark125@gmail.com>
2016-02-17 22:07:13 +0200

    adding a meteor package.js

--- package.js
+++ package.js
@@ -5,7 +5,7 @@
 
 Package.describe({
     name: 'mark125:jsmaps',
-    summary: 'Full featured interactive slippy map, and also an abstraction layer for popular javascript mapping apis. Supported Google Maps, Bing Maps, Yandex Maps and Here maps. ',
+    summary: 'A javascript map, and also with an abstraction layer for javascript mapping apis.',
     version: '0.0.12',
     git: 'https://github.com/markkr125/jsmaps.git',
     documentation: 'https://github.com/markkr125/jsmaps/blob/master/README.md'
@@ -14,3 +14,4 @@ Package.describe({
 Package.onUse(function (api) {
     api.versionsFrom('METEOR@1.0');
 });
+
